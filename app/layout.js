419385c1953daa5b2f038f66ea5b2cb3bf90d731
Ansh Rajpal <anshrajpal7@gmail.com>
2026-03-11 00:27:37 +0530

    google ad sense

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,16 @@
-import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
   title: "Zorado | The Daily Tech Digest",
   description: "A minimalist news aggregator for modern minds.",
+  other: {
+    "google-adsense-account": "ca-pub-9802160883492803",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9802160883492803"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
-      </head>
       <body className="antialiased bg-white selection:bg-neutral-200">
         {children}
       </body>
