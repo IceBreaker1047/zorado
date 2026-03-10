593f8f3e158f0d39f25eb1c81db20e2db3117b42
Ansh Rajpal <anshrajpal7@gmail.com>
2026-03-11 00:20:29 +0530

    google ad sense

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
-import "./globals.css";
 import Script from "next/script";
+import "./globals.css";
+
+export const metadata = {
+  title: "Zorado | The Daily Tech Digest",
+  description: "A minimalist news aggregator for modern minds.",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -12,7 +17,9 @@ export default function RootLayout({ children }) {
           strategy="afterInteractive"
         />
       </head>
-      <body>{children}</body>
+      <body className="antialiased bg-white selection:bg-neutral-200">
+        {children}
+      </body>
     </html>
   );
 }
